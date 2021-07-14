84ac536ce081a0d9f650516c7934734b2b5ba6c4
robertomacedo <macedorss20@gmail.com>
2021-07-14 16:29:16 -0300

    criado requiremnts.txt

---
+++ tasks/heroku.py
@@ -0,0 +1,14 @@
+import environ
+
+
+env = environ.Env()
+
+DEBUG = env.bool('DEBUG', False)
+
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+DATABASES = {
+    'default': env.db(),
+}
